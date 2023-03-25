624c7ec1244ab5e85325d39b205ae88af9cedb04
P1kaj1uu <891523233@qq.com>
2023-03-25 16:07:10 +0800

    优化路由懒加载，配置加载loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,55 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from '@/pages/Login'
-import Layout from '@/pages/Layout'
-import Home from '@/pages/Home';
-import Article from '@/pages/Article';
-import Publish from '@/pages/Publish';
+// 实现路由懒加载
+import { lazy, Suspense } from 'react'
 import './App.css';
 // 导入antd样式
 import 'antd/dist/reset.css';
 // 导入封装的路由鉴权高阶组件
 import AuthComponent from '@/components/AuthComponent'
+// 按需导入路由组件
+const Login = lazy(() => import('@/pages/Login'))
+const Layout = lazy(() => import('@/pages/Layout'))
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 
 function App() {
   return (
     // 路由配置
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          {/* 创建路由path和组件之间的对应关系 */}
-          <Route path='/login' element={<Login />}></Route>
-          {/* 需要鉴权的路由 */}
-          <Route path="/*" element={
-            <AuthComponent>
-              <Layout />
-            </AuthComponent>
-          } >
-            {/* 二级路由默认页面 */}
-            <Route index element={<Home />} />
-            <Route path="article" element={<Article />} />
-            <Route path="publish" element={<Publish />} />
-          </Route>
-        </Routes>
-      </div>
+      <Suspense
+        fallback={
+          <div className="loadEffect">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        }
+      >
+        <div className="App">
+          <Routes>
+            {/* 创建路由path和组件之间的对应关系 */}
+            <Route path='/login' element={<Login />}></Route>
+            {/* 需要鉴权的路由 */}
+            <Route path="/*" element={
+              <AuthComponent>
+                <Layout />
+              </AuthComponent>
+            } >
+              {/* 二级路由默认页面 */}
+              <Route index element={<Home />} />
+              <Route path="article" element={<Article />} />
+              <Route path="publish" element={<Publish />} />
+            </Route>
+          </Routes>
+        </div>
+      </Suspense>
     </BrowserRouter>
   );
 }
